1bb5efc0fa9c7a03dd29b81cdc450a245e8a976f
lindacnatt <46690041+lindacnatt@users.noreply.github.com>
2019-12-12 11:55:12 +0100

    add the first UI sketch

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let pg;
+
+function setup() {
+  createCanvas(710, 400);
+  pg = createGraphics(400, 250);
+}
+
+function draw() {
+  fill(0, 12);
+  rect(0, 0, width, height);
+  colorMode(HSB, 255);
+  let c = color(150, (height-mouseY)*255/height, 255);
+  fill(c);
+  noStroke();
+  ellipse(mouseX, mouseY, mouseX/log(mouseX),mouseX/log(mouseX));
+
+  pg.background(51);
+  pg.noFill();
+  pg.stroke(255);
+  pg.ellipse(mouseX - 150, mouseY - 75, 60, 60);
+}
