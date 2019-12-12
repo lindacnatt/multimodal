6594fffed8e724f5bc3de51f3de8d5c83af6c751
lindacnatt <linda.cnattingius@gmail.com>
2019-12-12 14:31:42 +0100

    merge calles och lindas kod

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+var cursor = {X: mouseX, Y: mouseY}
+
 function setup() {
   createCanvas(displayWidth, displayHeight);
 }
@@ -11,4 +13,30 @@ function draw() {
   noStroke();
   var pointerSize = (displayWidth-mouseX/1.5)/10;
   ellipse(mouseX, mouseY, pointerSize, pointerSize);
+}
+
+function avg(list){
+  let sum = values.reduce((previous, current) => current += previous);
+  let avg = sum / values.length;
+  return avg;
+}
+function changeX(Xvalue){
+  var listX = [0,0,0,0,0]
+  list.pop(0)
+  list.append(Xvalue)
+  return listX;
+}
+function changeY(Yvalue){
+  var listY = [0,0,0,0,0]
+  list.pop(0)
+  list.append(Yvalue)
+  return listY;
+}
+function avgMouse(cursorObject){
+  var X = changeX(cursorObject.X)
+  nX = avg(listX)
+  var Y = changeY(cursorObject.Y)
+  nY = avg(listY)
+  var newCursor = {newX:nX, newY: nY }
+  return newCursor;  
 }
