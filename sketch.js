31a8a9901ef67d867969a9572af78c96488b1837
carzet <carzet@kth.se>
2019-12-13 14:25:20 +0100

    Delat upp i belakompatibel sketch.js och webbläsar test sketchtest.js, samt tagit in hela p5 paketet i html
    Delat upp i belakompatibel sketch.js och webbläsar test sketchtest.js, samt tagit in hela p5 paketet i html

--- sketch.js
+++ sketch.js
@@ -1,32 +1,41 @@
+var guiSketch = new p5(( sketch ) => {
+
 let lastX = [];
 let lastY = [];
 
-function avg(t) {
+sketch.avg = function(t) {
   let sum = 0;
   for (let item of t) {
     sum += item;
-  }
+  };
   return sum / t.length;
-}
+};
 
-function setup() {
-  noCursor()
-  
-  createCanvas(displayWidth, displayHeight);
-}
+sketch.setup = function() {
+  sketch.noCursor();
+  sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+};
 
-function draw() {
-  fullscreen(true);
-  fill(0, 12);
-  rect(0, 0, width, height);
-  colorMode(HSB, 255);
-  let c = color(150, (height-mouseY*0.6)*255/height, 175); 
-  fill(c);
-  noStroke();
+sketch.draw = function() {
+  sketch.fullscreen(true);
+  sketch.fill(0, 12);
+  sketch.rect(0, 0, sketch.width, sketch.height);
+  sketch.colorMode(HSB, 255);
+  let c = sketch.color(150, (sketch.height-sketch.mouseY*0.6)*255/sketch.height, 175); 
+  sketch.fill(c);
+  sketch.noStroke();
   lastX.push(mouseX);
   lastY.push(mouseY);
-  var pointerSize = (displayWidth-mouseX/1.5)/10;
-  ellipse(avg(lastX), avg(lastY), pointerSize, pointerSize);
+  var pointerSize = (sketch.windowWidth-sketch.mouseX/1.5)/10;
+  sketch.ellipse(sketch.avg(lastX), sketch.avg(lastY), pointerSize, pointerSize);
   lastX = lastX.slice(lastX.length-30);
   lastY = lastY.slice(lastY.length-30);
-}
+};
+  
+  sketch.mouseMoved = function() {
+    let mouseX = sketch.mouseX/sketch.width;
+    let mouseY = sketch.mouseY/sketch.height;
+    Bela.data.sendBuffer(0, 'float', [mouseX, mouseY]);
+    Bela.control.send({mouseX: mouseX, mouseY: mouseY});
+  };
+}, 'gui');
---
+++ sketchtest.js
@@ -0,0 +1,32 @@
+let lastX = [];
+let lastY = [];
+
+function avg(t) {
+  let sum = 0;
+  for (let item of t) {
+    sum += item;
+  }
+  return sum / t.length;
+}
+
+function setup() {
+  noCursor()
+  
+  createCanvas(displayWidth, displayHeight);
+}
+
+function draw() {
+  fullscreen(true);
+  fill(0, 12);
+  rect(0, 0, width, height);
+  colorMode(HSB, 255);
+  let c = color(150, (height-mouseY*0.6)*255/height, 175); 
+  fill(c);
+  noStroke();
+  lastX.push(mouseX);
+  lastY.push(mouseY);
+  var pointerSize = (displayWidth-mouseX/1.5)/10;
+  ellipse(avg(lastX), avg(lastY), pointerSize, pointerSize);
+  lastX = lastX.slice(lastX.length-30);
+  lastY = lastY.slice(lastY.length-30);
+}
