d8a708a13594f15e5ae0603c96701be3b13aee5b
lindacnatt <linda.cnattingius@gmail.com>
2020-01-07 13:12:20 +0100

    Update color of lines and change position of tone text

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ var guiSketch = new p5(( sketch ) => {
 let lastX = [];
 let lastY = [];
 let toneArray = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+let colorArray = [0, 23, 49, 89,  113, 159, 199, 222, 251, 274, 299, 324]
 sketch.avg = function(t) {
   let sum = 0;
   for (let item of t) {
@@ -13,21 +14,22 @@ sketch.avg = function(t) {
 
 sketch.grid = function (n) {
   sketch.fill(255);
+  sketch.colorMode(HSB, 360);
   let tone = 0;
   let colorValue = 0;
   for (var x = 0; x < sketch.windowWidth; x += sketch.windowWidth /n) {
+    sketch.stroke(sketch.color(colorArray[colorValue], 200, 200));
+    sketch.strokeWeight(1);
+    sketch.line(x, 0, x, windowHeight);
+    if (colorValue == 12) {
+      colorValue = 0;
+    }
+    colorValue += 1;
     sketch.textSize(26);
     sketch.text(toneArray[tone], x + 10, sketch.windowHeight);
     tone += 1;
     if (tone == 12) {
       tone = 0
-    };
-    colorValue = colorValue + 20;
-    sketch.stroke(colorValue, 100, 100);
-    sketch.strokeWeight(1);
-    sketch.line(x, 0, x, sketch.windowHeight);
-    if (x == sketch.windowWidth / 2) {
-      colorValue = 0;
     };
   };
 }
--- sketchtest.js
+++ sketchtest.js
@@ -11,16 +11,15 @@ function avg(t) {
 
 function setup() {
   noCursor()
-
   createCanvas(displayWidth, displayHeight);
+  fullscreen(true);
 }
 
 function draw() {
-  fullscreen(true);
   fill(0, 12);
   rect(0, 0, width, height);
   colorMode(HSB, 255);
-  let c = color(150, (height - mouseY * 0.6) * 255 / height, 175);
+  let c = color(150, (height - mouseY) * 255 / height, 175);
   fill(c);
   noStroke();
   lastX.push(mouseX);
@@ -30,23 +29,25 @@ function draw() {
   lastX = lastX.slice(lastX.length - 30);
   lastY = lastY.slice(lastY.length - 30);
   let toneArray = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+  let colorArray = [0, 23, 49, 89,  113, 159, 199, 222, 251, 274, 299, 324]
   grid = function () {
     fill(255);
+    colorMode(HSB, 360);
     let tone = 0;
     let colorValue = 0;
     for (var x = 0; x < windowWidth; x += windowWidth / 24) {
+      stroke(color(colorArray[colorValue], 200, 200));
+      strokeWeight(1);
+      line(x, 0, x, windowHeight);
+      if (colorValue == 12) {
+        colorValue = 0;
+      }
+      colorValue += 1;
       textSize(26);
-      text(toneArray[tone], x + 10, windowHeight);
+      text(toneArray[tone], x - 9, windowHeight);
       tone += 1;
       if (tone == 12) {
         tone = 0
-      }
-      colorValue = colorValue + 20;
-      stroke(colorValue, 100, 100);
-      strokeWeight(1);
-      line(x, 0, x, windowHeight);
-      if (x == windowWidth / 2) {
-        colorValue = 0;
       }
     };
   }
